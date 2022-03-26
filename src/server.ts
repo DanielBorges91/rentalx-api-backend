ac082f1153c3f3958ff1e613e28f4594d910a917
Daniel Borges <danprog91@gmail.com>
2022-03-26 17:49:06 -0300

    config debug

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => response.json({ message: "Hello World!" }));
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => {
   console.log("🚀 Server started on port 3333!");
