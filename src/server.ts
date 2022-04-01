9bdf997ae52554925e160c392ec2de5389968a3d
Daniel Borges <danprog91@gmail.com>
2022-04-01 20:29:08 -0300

    refatored inject dependence of createCategory

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import "reflect-metadata";
+
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+
+import "./shared/container";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
@@ -15,5 +19,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333!");
+  console.log("🚀 Server started!");
 });
