be92e749bc8626633d08371dd59308ec3a061c87
Daniel Borges <danprog91@gmail.com>
2022-04-10 20:56:58 -0300

    implemented list cars available

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,5 +4,10 @@ import { Car } from "../infra/typeorm/entities/Cars";
 interface ICarsRepository {
   create(data: ICreateCarsDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string
+  ): Promise<Car[]>;
 }
 export { ICarsRepository };
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -38,5 +38,25 @@ class CarsRepositoryInMemory implements ICarsRepository {
     );
     return carAlreadyExists;
   }
+
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsAvailable = this.cars.filter((car) => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+      return null;
+    });
+
+    return carsAvailable;
+  }
 }
 export { CarsRepositoryInMemory };
